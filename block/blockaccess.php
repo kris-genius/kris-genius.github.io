019e2e190859eda9d619c7f8bea561ca4cb83bfb
Kris Genius <114202204+kris-genius@users.noreply.github.com>
2023-07-02 16:05:36 +0700

    Add files via upload

---
+++ block/blockaccess.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>Access Blocked</title>
+</head>
+<body>
+   <h1>Access Blocked</h1>
+   <p>Maaf, akses ke halaman ini diblokir.</p>
+</body>
+</html>
